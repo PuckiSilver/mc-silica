6fed4b93056b87e6a1f9327eba1e73d21dcc0bbd
PuckiSilver <pucki.silver@gmail.com>
2022-11-30 23:14:30 +0100

    start implementing model generation plugin

--- plugins/silica.py
+++ plugins/silica.py
@@ -1,10 +1,11 @@
+from typing import List, overload
 from beet import Context, Texture
 from PIL import Image
+from beet.library.resource_pack import Model
 
-def generateImage(overlay):
-    size = tuple(overlay.get('size', [16, 16]))
-    cdMax = overlay.get('cdMax', 15)
-    shape = overlay.get('shape', "linear")
+def generateImage(sizeIn: list, cdMax: float, shape: str):
+    if sizeIn is None or cdMax is None or shape is None: return None
+    size = tuple(sizeIn)
 
     green = 0
     if cdMax < 10:
@@ -29,10 +30,45 @@ def generateImage(overlay):
 
     return Texture(overlay)
 
-def generateOverlay(ctx: Context):
-    overlays = ctx.meta.get('mc-silica', [])
+def generateTexture(ctx: Context):
+    meta = ctx.meta.get('mc-silica')
+    if meta is None: return
+    overlays = meta.get('textures')
+    if not isinstance(overlays, list): return
 
     for overlay in overlays:
-        save = overlay.get('save', "")
-        if save == "": continue
-        ctx.assets.textures[save] = generateImage(overlay)
+        path = overlay.get('path')
+        if path is None: continue
+
+        texture = generateImage(
+            overlay.get('size'),
+            overlay.get('cdMax'),
+            overlay.get('shape'))
+        if texture is None: continue
+        ctx.assets.textures[path] = texture
+
+def insertPlane(model: Model, overlay: str):
+    model.data['textures']['layer0'] = overlay
+
+    # SHOULD ONLY WORK FOR MODELS WITH
+    # at max 2 rotations set to a multiple of 22.5
+    # negative values are the same spiel
+    # 3rd rotation can be any multiple of 90
+
+    return model
+
+def generateModel(ctx: Context):
+    meta = ctx.meta.get('mc-silica')
+    if meta is None: return
+    models = meta.get('models')
+    if not isinstance(models, list): return
+
+    for model in models:
+        path = model.get('path')
+        overlay = model.get('overlay')
+        if path is None or overlay is None: continue
+        newModel = ctx.assets.models[path]
+        newModel = insertPlane(newModel, overlay)
+
+
+
