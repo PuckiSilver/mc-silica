eeaed0fb6e78f701697afc301a0a7276b782171b
PuckiSilver <pucki.silver@gmail.com>
2022-12-16 23:22:16 +0100

    💀 remove dead code

--- plugins/silica.py
+++ plugins/silica.py
@@ -1,7 +1,5 @@
-from typing import List, overload
 from beet import Context, Texture
 from PIL import Image
-from beet.library.resource_pack import Model
 
 def generateImage(sizeIn: list, cdMax: float, shape: str):
     if sizeIn is None or cdMax is None or shape is None: return None
@@ -46,29 +44,3 @@ def generateTexture(ctx: Context):
             overlay.get('shape'))
         if texture is None: continue
         ctx.assets.textures[path] = texture
-
-def insertPlane(model: Model, overlay: str):
-    model.data['textures']['layer0'] = overlay
-
-    # SHOULD ONLY WORK FOR MODELS WITH
-    # at max 2 rotations set to a multiple of 22.5
-    # negative values are the same spiel
-    # 3rd rotation can be any multiple of 90
-
-    return model
-
-def generateModel(ctx: Context):
-    meta = ctx.meta.get('mc-silica')
-    if meta is None: return
-    models = meta.get('models')
-    if not isinstance(models, list): return
-
-    for model in models:
-        path = model.get('path')
-        overlay = model.get('overlay')
-        if path is None or overlay is None: continue
-        newModel = ctx.assets.models[path]
-        newModel = insertPlane(newModel, overlay)
-
-
-
